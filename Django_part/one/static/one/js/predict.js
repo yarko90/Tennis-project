d7ad1128d71ea8cf9b0b58a34b52e0bfd079303f
yarko90 <yaroskoc@yandex.ru>
2016-02-18 15:37:50 +0300

    Проект
    Rename

---
+++ Django_part/one/static/one/js/predict.js
@@ -0,0 +1,38 @@
+function goPredict(){
+    //alert('goPredict');
+    $("#content").load("predictions/ #predict")
+}
+function goStrategy(){
+    //alert('goPredict');
+    $("#content").load("strategy/ #strategy")
+}
+function goAbout(){
+    //alert('goPredict');
+    $("#content").load("about/ #about")
+}
+function goAboutFromPredictions(){
+    //alert('goPredict');
+    $("#content").load("about/ #about")
+}
+
+$('#strategy1').on('click',function(){
+    $("#gameTable").load("../variants/ #strategy1")
+})
+$('#strategy2').on('click',function(){
+    $("#gameTable").load("../variants/ #strategy2")
+})
+$('#strategy3').on('click',function(){
+    $("#gameTable").load("../variants/ #strategy3")
+})
+
+
+/*document.getElementById("strategy1").onclick=function(){
+    alert("Стратегия1")
+}
+document.getElementById("strategy2").onclick=function(){
+    alert("Стратегия2")
+}
+document.getElementById("strategy3").onclick=function(){
+    alert("Стратегия3")
+}
+*/
